5a6630f6dbe577fe1d2adb2dcf9b964aa1a17e50
den <den.matvssnko.99@ukr.net>
2022-06-26 21:40:10 +0100

    initial common

---
+++ common/Link/Link.tsx
@@ -0,0 +1,20 @@
+import { FC, ReactNode } from "react";
+import NextLink from 'next/link'
+import { LinkStyled, LinkColor } from './LinkStyled'
+
+export interface LinkProps {
+  href?: string;
+  children: ReactNode;
+  color?: LinkColor.primary | LinkColor.secondary
+  className?: any;
+  download?: boolean;
+  target?: string;
+}
+
+export const Link: FC<LinkProps> =  ({href = '', children, color = LinkColor.primary, className, download, target}) => (
+  <NextLink href={href} passHref>
+    <LinkStyled className={className} download={download} target={target} color={color}>
+      {children}
+    </LinkStyled>
+  </NextLink>
+);
---
+++ common/Link/LinkStyled.ts
@@ -0,0 +1,15 @@
+import styled from '@emotion/styled';
+import { LinkProps } from './Link';
+
+export enum LinkColor {
+  primary = 'primary',
+  secondary = 'secondary',
+}
+
+export const LinkStyled = styled('a')<LinkProps>(({color, theme}: any) => ({
+  color: color === LinkColor.primary ? '#4BB4B4' : 'white',//theme?.palette?.primary
+
+  ':hover': {
+    textDecoration: 'underline',
+  }
+}));
