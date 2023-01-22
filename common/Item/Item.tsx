ffc9b891eef5b04d00f2ff6730587eda83a44e3c
borisigorevich <borysigorevich@gmail.com>
2023-01-23 01:43:29 +0200

    test fix

--- common/Item/Item.tsx
+++ common/Item/Item.tsx
@@ -1,15 +1,16 @@
 import { ReactNode } from "react";
 
 import { SxProps, Theme } from "@mui/material";
-import Grid from "@mui/material/Grid";
+import Grid, {GridProps} from "@mui/material/Grid";
 
-interface ItemProps {
-  children: ReactNode,
-  spacing?: number,
-  sx?: SxProps<Theme>;
-}
+// interface ItemProps {
+//   children: ReactNode,
+//   spacing?: number,
+//   sx?: SxProps<Theme>;
+//   item: number
+// }
 
-export const Item = ({children, ...rest}: ItemProps) => (
+export const Item = ({children, ...rest}: GridProps) => (
   <Grid {...rest}>
     {children}
   </Grid>
